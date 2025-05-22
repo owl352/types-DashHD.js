6ca3c5acbe90eb6a28abde1be9e410c73b49f022
owl352 <kokosinca123@gmail.com>
2025-05-22 12:51:03 +0300

    enums fix

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,23 @@
 export = DashHD;
 
 declare namespace DashHD {
-    enum Versions {
-        'mainnet' = 'mainnet',
-        'testnet' = 'testnet',
-        'DASH_PKH' = 'DASH_PKH',
-        'DASH_PRIV_KEY' = 'DASH_PRIV_KEY',
-        'DASH_PKH_TESTNET' = 'DASH_PKH_TESTNET',
-        'DASH_PRIV_KEY_TESTNET' = 'DASH_PRIV_KEY_TESTNET',
-        'xprv' = 'xprv',
-        'tprv' = 'tprv',
-        'xpub' = 'xpub',
-        'tpub' = 'tpub',
-        'XPRV' = 'XPRV',
-        'XPUB' = 'XPUB',
-        'TPRV' = 'TPRV',
-        'TPUB' = 'TPUB',
-    }
-    
-    enum KeyType {
-        'private' = 'private',
-        'pkh' = 'pkh',
-        'xprv' = 'xprv',
-        'xpub' = 'xpub',
-        '' = ''
-    }
+    type Versions =
+        'mainnet'
+        | 'testnet'
+        | 'DASH_PKH'
+        | 'DASH_PRIV_KEY'
+        | 'DASH_PKH_TESTNET'
+        | 'DASH_PRIV_KEY_TESTNET'
+        | 'xprv'
+        | 'tprv'
+        | 'xpub'
+        | 'tpub'
+        | 'XPRV'
+        | 'XPUB'
+        | 'TPRV'
+        | 'TPUB'
+
+    type KeyType = 'private' | 'pkh' | 'xprv' | 'xpub' | ''
 
     type HDXKeyPartial = {
         deriveAddress: (index: number) => Promise<HDKey>
@@ -109,34 +102,62 @@ declare namespace DashHD {
     const HARDENED_OFFSET: number
 
     function create(opts?: HDKeyOptions): HDKey
+
     function toAddr(pubBytes: Uint8Array, opts?: { version: Versions | number }): Promise<string>
-    function toWif (privBytes: Uint8Array, opts?: { version: Versions | number }): Promise<string>
-    function toXPrv (hdkey: HDKey, opts?: { version: Versions | number }): Promise<string>
-    function toXPrvBytes (hdkey: HDKey, opts?: { version: Versions | number }): Promise<Uint8Array>
-    function toXPub (hdkey: HDKey, opts?: { version: Versions | number }): Promise<Uint8Array>
-    function toXPubBytes (hdkey: HDKey, opts?: { version: Versions | number }): Promise<Uint8Array>
-    function deriveChild (hdkey: HDKey, index: number, hardened: boolean): Promise<HDKey>
-    function derivePath (parent: HDKey, path: string): Promise<HDKey>
-    function fromSeed (seed: Uint8Array, opts?: HDFromSeedOptions): Promise<HDWallet>
-    function fromXKey (xkey: string, opts?: HDFromXKeyOptions): Promise<HDXKey>
-    function toId (hdkey: HDKey): Promise<string>
-    function toIdBytes (hdkey: HDKey): Promise<Uint8Array>
-    function toPublic (hdkey: HDKey): HDKey
-    function wipePrivateData (hdkey: HDKey): Promise<HDKey>
+
+    function toWif(privBytes: Uint8Array, opts?: { version: Versions | number }): Promise<string>
+
+    function toXPrv(hdkey: HDKey, opts?: { version: Versions | number }): Promise<string>
+
+    function toXPrvBytes(hdkey: HDKey, opts?: { version: Versions | number }): Promise<Uint8Array>
+
+    function toXPub(hdkey: HDKey, opts?: { version: Versions | number }): Promise<Uint8Array>
+
+    function toXPubBytes(hdkey: HDKey, opts?: { version: Versions | number }): Promise<Uint8Array>
+
+    function deriveChild(hdkey: HDKey, index: number, hardened: boolean): Promise<HDKey>
+
+    function derivePath(parent: HDKey, path: string): Promise<HDKey>
+
+    function fromSeed(seed: Uint8Array, opts?: HDFromSeedOptions): Promise<HDWallet>
+
+    function fromXKey(xkey: string, opts?: HDFromXKeyOptions): Promise<HDXKey>
+
+    function toId(hdkey: HDKey): Promise<string>
+
+    function toIdBytes(hdkey: HDKey): Promise<Uint8Array>
+
+    function toPublic(hdkey: HDKey): HDKey
+
+    function wipePrivateData(hdkey: HDKey): Promise<HDKey>
 
 
-    function decode (base58key: string, opts?: {versions: [string, string], xversions: [string, string]}): Promise<Parts>
-    function encodeXPrv (keyBytes: Uint8Array, opts?: { version: Versions | number }): Promise<string>
-    function encodeXPub (keyBytes: Uint8Array, opts?: { version: Versions | number }): Promise<string>
-    function privateKeyTweakAdd (privateKeyBytes: Uint8Array, tweakBytes: Uint8Array): Promise<Uint8Array>
-    function publicKeyNormalize (pubBytes: Uint8Array): Promise<Uint8Array>
-    function publicKeyTweakAdd (publicKeyBytes: Uint8Array, tweakBytes: Uint8Array): Promise<Uint8Array>
-    function ripemd160sum (bytes: Uint8Array): Promise<Uint8Array>
-    function sha256sum (bytes: Uint8Array): Promise<Uint8Array>
-    function sha512hmac (entropy: Uint8Array, data: Uint8Array): Promise<Uint8Array>
-    function bytesToBase64Url (bytes: Uint8Array): string
-    function secureErase (buf: Uint8Array): void
-    function toPublicKey (privBytes: Uint8Array): Promise<Uint8Array>
+    function decode(base58key: string, opts?: {
+        versions: [string, string],
+        xversions: [string, string]
+    }): Promise<Parts>
+
+    function encodeXPrv(keyBytes: Uint8Array, opts?: { version: Versions | number }): Promise<string>
+
+    function encodeXPub(keyBytes: Uint8Array, opts?: { version: Versions | number }): Promise<string>
+
+    function privateKeyTweakAdd(privateKeyBytes: Uint8Array, tweakBytes: Uint8Array): Promise<Uint8Array>
+
+    function publicKeyNormalize(pubBytes: Uint8Array): Promise<Uint8Array>
+
+    function publicKeyTweakAdd(publicKeyBytes: Uint8Array, tweakBytes: Uint8Array): Promise<Uint8Array>
+
+    function ripemd160sum(bytes: Uint8Array): Promise<Uint8Array>
+
+    function sha256sum(bytes: Uint8Array): Promise<Uint8Array>
+
+    function sha512hmac(entropy: Uint8Array, data: Uint8Array): Promise<Uint8Array>
+
+    function bytesToBase64Url(bytes: Uint8Array): string
+
+    function secureErase(buf: Uint8Array): void
+
+    function toPublicKey(privBytes: Uint8Array): Promise<Uint8Array>
 
     interface Utils {
         decode: typeof decode,
